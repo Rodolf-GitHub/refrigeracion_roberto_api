bf26c551f42d0fce40babeed127c437fe7d213f9
Rodolfo <rodolfogroero2@gmail.com>
2026-02-27 15:47:01 -0300

    Agregar hosts permitidos en la configuración de Django para el entorno de desarrollo

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,12 @@ SECRET_KEY = 'django-insecure-=1!akoe$k62=wpk0y++d&no_vg4#^c*%8%4)@=4v+-fw!7j1^-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'api.refrigeracionroberto.com',
+    'refrigeracionroberto.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
