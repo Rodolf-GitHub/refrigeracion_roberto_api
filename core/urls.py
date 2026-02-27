b0efd7fc6da9b39310e285fcac745778c6edf686
Rodolfo <rodolfogroero2@gmail.com>
2026-02-27 15:55:43 -0300

    Agregar encabezados y títulos personalizados para el panel de administración de Django

--- core/urls.py
+++ core/urls.py
@@ -19,6 +19,10 @@ from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header = 'Administración de RefrigeracionRoberto'
+admin.site.site_title = 'RefrigeracionRoberto Admin'
+admin.site.index_title = 'Panel de administración'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('proyectos.urls')),
